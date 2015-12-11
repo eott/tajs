621f4c6806d19f4ed967417262b90e9082625c02
Etienne Ott <eti.ott@gmail.com>
2015-12-11 22:00:15 +0100

    Implement output generation for util with text, description and options.

--- library/tajs/util.js
+++ library/tajs/util.js
@@ -1,3 +1,11 @@
+$.extend({
+    prepareText: function(txt) {
+        return ('' + txt).replace('\r', '')
+            .replace(/[\r\n]/g, '<br/>')
+            .replace('"','\\"');
+    }
+});
+
 $(document).ready(function() {
     // Attach event listener for adding options
     $('button[name="addOption"]').on('click', function(event) {
@@ -17,5 +25,38 @@ $(document).ready(function() {
 
         // Done, append
         $('select[name="options"]').append(element);
+    });
+
+    // Attach event listener for creating the output
+    $('button[name="createOutput"]').on('click', function(event) {
+        // Buttons want to submit the form. Not on my watch!
+        event.preventDefault();
+
+        // The following get's a bit ugly, we basically just want a reasonably
+        // pretty and escaped string to dump into the output textarea
+        var template = '{' + "\n"
+            +'    "name": "%name%",' + "\n"
+            +'    "description": "%desc%",' + "\n"
+            +'    "options": [' + "\n"
+            +'        %options%' + "\n"
+            +'    ]' + "\n"
+            +'}' + "\n";
+
+        template = template.replace('%name%', $('input[name="sceneName"]').val());
+        template = template.replace('%desc%', $.prepareText($('textarea[name="description"]').val()));
+
+        var opts = "";
+        $('select[name="options"]').children().each(function(i, child) {
+            if (opts.length > 0) {
+                opts += ',';
+            }
+            opts += '{"text":"' + $.prepareText($(child).data('text'))
+                + '","proceed":' + $.prepareText($(child).data('proceed'))
+                + '"}';
+        });
+        opts = opts.replace('/.*,$/gm', '');
+        template = template.replace('%options%', opts);
+
+        $('textarea[name="output"]').val(template);
     });
 });
